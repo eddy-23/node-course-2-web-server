c621e41ffeac3c903bc0afd86cbe766d47f35a99
Alive <alive@MBP-Alive.local>
2018-10-04 12:57:45 +0300

    New features

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    })
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
